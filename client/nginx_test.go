1900f9fbd86f9566873dad20110c1549242c73f3
Dean Coakley <dean.s.coakley@gmail.com>
2018-09-25 18:09:37 +0100

    Fix go style (#11)
    * Run test files through gofmt -s found by goreportcard
    * Fix indent

--- client/nginx_test.go
+++ client/nginx_test.go
@@ -13,96 +13,96 @@ func TestDetermineUpdates(t *testing.T) {
 		expectedToDelete []UpstreamServer
 	}{{
 		updated: []UpstreamServer{
-			UpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			},
-			UpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			},
 		},
 		nginx: []UpstreamServer{
-			UpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			UpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
 		},
 		expectedToAdd: []UpstreamServer{
-			UpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			},
-			UpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			},
 		},
 		expectedToDelete: []UpstreamServer{
-			UpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			UpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
 		}}, {
 		updated: []UpstreamServer{
-			UpstreamServer{
+			{
 				Server: "10.0.0.2:80",
 			},
-			UpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			},
-			UpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			},
 		},
 		nginx: []UpstreamServer{
-			UpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			UpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
-			UpstreamServer{
+			{
 				ID:     3,
 				Server: "10.0.0.3:80",
 			},
 		},
 		expectedToAdd: []UpstreamServer{
-			UpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			}},
 		expectedToDelete: []UpstreamServer{
-			UpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			}},
 	}, {
 		updated: []UpstreamServer{
-			UpstreamServer{
+			{
 				Server: "10.0.0.1:80",
 			},
-			UpstreamServer{
+			{
 				Server: "10.0.0.2:80",
 			},
-			UpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			}},
 		nginx: []UpstreamServer{
-			UpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			UpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
-			UpstreamServer{
+			{
 				ID:     3,
 				Server: "10.0.0.3:80",
 			},
@@ -126,96 +126,96 @@ func TestStreamDetermineUpdates(t *testing.T) {
 		expectedToDelete []StreamUpstreamServer
 	}{{
 		updated: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			},
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			},
 		},
 		nginx: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			StreamUpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
 		},
 		expectedToAdd: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			},
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			},
 		},
 		expectedToDelete: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			StreamUpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
 		}}, {
 		updated: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.2:80",
 			},
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			},
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			},
 		},
 		nginx: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			StreamUpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
-			StreamUpstreamServer{
+			{
 				ID:     3,
 				Server: "10.0.0.3:80",
 			},
 		},
 		expectedToAdd: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.4:80",
 			}},
 		expectedToDelete: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			}},
 	}, {
 		updated: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.1:80",
 			},
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.2:80",
 			},
-			StreamUpstreamServer{
+			{
 				Server: "10.0.0.3:80",
 			}},
 		nginx: []StreamUpstreamServer{
-			StreamUpstreamServer{
+			{
 				ID:     1,
 				Server: "10.0.0.1:80",
 			},
-			StreamUpstreamServer{
+			{
 				ID:     2,
 				Server: "10.0.0.2:80",
 			},
-			StreamUpstreamServer{
+			{
 				ID:     3,
 				Server: "10.0.0.3:80",
 			},
