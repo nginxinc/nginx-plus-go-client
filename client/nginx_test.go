e5ee0793c3c04c45ec00b848ba314b896e946eb2
Vighneswar Rao Bojja <v.bojja@f5.com>
2019-10-09 12:10:31 +0530

    Add support for updating server parameters

--- client/nginx_test.go
+++ client/nginx_test.go
@@ -6,11 +6,13 @@ import (
 )
 
 func TestDetermineUpdates(t *testing.T) {
+	maxConns := 1
 	var tests = []struct {
 		updated          []UpstreamServer
 		nginx            []UpstreamServer
 		expectedToAdd    []UpstreamServer
 		expectedToDelete []UpstreamServer
+		expectedToUpdate []UpstreamServer
 	}{{
 		updated: []UpstreamServer{
 			{
@@ -95,35 +97,65 @@ func TestDetermineUpdates(t *testing.T) {
 			}},
 		nginx: []UpstreamServer{
 			{
-				ID:     1,
 				Server: "10.0.0.1:80",
 			},
 			{
-				ID:     2,
 				Server: "10.0.0.2:80",
 			},
 			{
-				ID:     3,
 				Server: "10.0.0.3:80",
 			},
 		}}, {
 		// empty values
-	}}
+	},
+		{
+			updated: []UpstreamServer{
+				{
+					Server:   "10.0.0.1:80",
+					MaxConns: &maxConns,
+				},
+			},
+			nginx: []UpstreamServer{
+				{
+					ID:     1,
+					Server: "10.0.0.1:80",
+				},
+				{
+					ID:     2,
+					Server: "10.0.0.2:80",
+				},
+			},
+			expectedToDelete: []UpstreamServer{
+				{
+					ID:     2,
+					Server: "10.0.0.2:80",
+				},
+			},
+			expectedToUpdate: []UpstreamServer{
+				{
+					ID:       1,
+					Server:   "10.0.0.1:80",
+					MaxConns: &maxConns,
+				},
+			}},
+	}
 
 	for _, test := range tests {
-		toAdd, toDelete := determineUpdates(test.updated, test.nginx)
-		if !reflect.DeepEqual(toAdd, test.expectedToAdd) || !reflect.DeepEqual(toDelete, test.expectedToDelete) {
-			t.Errorf("determiteUpdates(%v, %v) = (%v, %v)", test.updated, test.nginx, toAdd, toDelete)
+		toAdd, toDelete, toUpdate := determineUpdates(test.updated, test.nginx)
+		if !reflect.DeepEqual(toAdd, test.expectedToAdd) || !reflect.DeepEqual(toDelete, test.expectedToDelete) || !reflect.DeepEqual(toUpdate, test.expectedToUpdate) {
+			t.Errorf("determineUpdates(%v, %v) = (%v, %v, %v)", test.updated, test.nginx, toAdd, toDelete, toUpdate)
 		}
 	}
 }
 
 func TestStreamDetermineUpdates(t *testing.T) {
+	maxConns := 1
 	var tests = []struct {
 		updated          []StreamUpstreamServer
 		nginx            []StreamUpstreamServer
 		expectedToAdd    []StreamUpstreamServer
 		expectedToDelete []StreamUpstreamServer
+		expectedToUpdate []StreamUpstreamServer
 	}{{
 		updated: []StreamUpstreamServer{
 			{
@@ -221,12 +253,248 @@ func TestStreamDetermineUpdates(t *testing.T) {
 			},
 		}}, {
 		// empty values
-	}}
+	},
+		{
+			updated: []StreamUpstreamServer{
+				{
+					Server:   "10.0.0.1:80",
+					MaxConns: &maxConns,
+				},
+			},
+			nginx: []StreamUpstreamServer{
+				{
+					ID:     1,
+					Server: "10.0.0.1:80",
+				},
+				{
+					ID:     2,
+					Server: "10.0.0.2:80",
+				},
+			},
+			expectedToDelete: []StreamUpstreamServer{
+				{
+					ID:     2,
+					Server: "10.0.0.2:80",
+				},
+			},
+			expectedToUpdate: []StreamUpstreamServer{
+				{
+					ID:       1,
+					Server:   "10.0.0.1:80",
+					MaxConns: &maxConns,
+				},
+			}},
+	}
 
 	for _, test := range tests {
-		toAdd, toDelete := determineStreamUpdates(test.updated, test.nginx)
-		if !reflect.DeepEqual(toAdd, test.expectedToAdd) || !reflect.DeepEqual(toDelete, test.expectedToDelete) {
-			t.Errorf("determiteUpdates(%v, %v) = (%v, %v)", test.updated, test.nginx, toAdd, toDelete)
+		toAdd, toDelete, toUpdate := determineStreamUpdates(test.updated, test.nginx)
+		if !reflect.DeepEqual(toAdd, test.expectedToAdd) || !reflect.DeepEqual(toDelete, test.expectedToDelete) || !reflect.DeepEqual(toUpdate, test.expectedToUpdate) {
+			t.Errorf("determiteUpdates(%v, %v) = (%v, %v, %v)", test.updated, test.nginx, toAdd, toDelete, toUpdate)
+		}
+	}
+}
+
+func TestAddPortToServer(t *testing.T) {
+	// More info about addresses http://nginx.org/en/docs/http/ngx_http_upstream_module.html#server
+	var tests = []struct {
+		address  string
+		expected string
+		msg      string
+	}{
+		{
+			address:  "example.com:8080",
+			expected: "example.com:8080",
+			msg:      "host and port",
+		},
+		{
+			address:  "127.0.0.1:8080",
+			expected: "127.0.0.1:8080",
+			msg:      "ipv4 and port",
+		},
+		{
+			address:  "[::]:8080",
+			expected: "[::]:8080",
+			msg:      "ipv6 and port",
+		},
+		{
+			address:  "unix:/path/to/socket",
+			expected: "unix:/path/to/socket",
+			msg:      "unix socket",
+		},
+		{
+			address:  "example.com",
+			expected: "example.com:80",
+			msg:      "host without port",
+		},
+		{
+			address:  "127.0.0.1",
+			expected: "127.0.0.1:80",
+			msg:      "ipv4 without port",
+		},
+		{
+			address:  "[::]",
+			expected: "[::]:80",
+			msg:      "ipv6 without port",
+		},
+	}
+
+	for _, test := range tests {
+		result := addPortToServer(test.address)
+		if result != test.expected {
+			t.Errorf("addPortToServer(%v) returned %v but expected %v for %v", test.address, result, test.expected, test.msg)
+		}
+	}
+}
+
+func TestHaveSameParameters(t *testing.T) {
+	tests := []struct {
+		server    UpstreamServer
+		serverNGX UpstreamServer
+		expected  bool
+	}{
+		{
+			server:    UpstreamServer{},
+			serverNGX: UpstreamServer{},
+			expected:  true,
+		},
+		{
+			server:    UpstreamServer{ID: 2},
+			serverNGX: UpstreamServer{ID: 3},
+			expected:  true,
+		},
+		{
+			server: UpstreamServer{},
+			serverNGX: UpstreamServer{
+				MaxConns:    &defaultMaxConns,
+				MaxFails:    &defaultMaxFails,
+				FailTimeout: defaultFailTimeout,
+				SlowStart:   defaultSlowStart,
+				Backup:      &defaultBackup,
+				Weight:      &defaultWeight,
+				Down:        &defaultDown,
+			},
+			expected: true,
+		},
+		{
+			server: UpstreamServer{
+				ID:          1,
+				Server:      "127.0.0.1",
+				MaxConns:    &defaultMaxConns,
+				MaxFails:    &defaultMaxFails,
+				FailTimeout: defaultFailTimeout,
+				SlowStart:   defaultSlowStart,
+				Backup:      &defaultBackup,
+				Weight:      &defaultWeight,
+				Down:        &defaultDown,
+			},
+			serverNGX: UpstreamServer{
+				ID:          1,
+				Server:      "127.0.0.1",
+				MaxConns:    &defaultMaxConns,
+				MaxFails:    &defaultMaxFails,
+				FailTimeout: defaultFailTimeout,
+				SlowStart:   defaultSlowStart,
+				Backup:      &defaultBackup,
+				Weight:      &defaultWeight,
+				Down:        &defaultDown,
+			},
+			expected: true,
+		},
+		{
+			server:    UpstreamServer{SlowStart: "10s"},
+			serverNGX: UpstreamServer{},
+			expected:  false,
+		},
+		{
+			server:    UpstreamServer{},
+			serverNGX: UpstreamServer{SlowStart: "10s"},
+			expected:  false,
+		},
+		{
+			server:    UpstreamServer{SlowStart: "20s"},
+			serverNGX: UpstreamServer{SlowStart: "10s"},
+			expected:  false,
+		},
+	}
+
+	for _, test := range tests {
+		result := haveSameParameters(test.server, test.serverNGX)
+		if result != test.expected {
+			t.Errorf("haveSameParameters(%v, %v) returned %v but expected %v", test.server, test.serverNGX, result, test.expected)
+		}
+	}
+}
+
+func TestHaveSameParametersForStream(t *testing.T) {
+	tests := []struct {
+		server    StreamUpstreamServer
+		serverNGX StreamUpstreamServer
+		expected  bool
+	}{
+		{
+			server:    StreamUpstreamServer{},
+			serverNGX: StreamUpstreamServer{},
+			expected:  true,
+		},
+		{
+			server:    StreamUpstreamServer{ID: 2},
+			serverNGX: StreamUpstreamServer{ID: 3},
+			expected:  true,
+		},
+		{
+			server: StreamUpstreamServer{},
+			serverNGX: StreamUpstreamServer{
+				MaxConns:    &defaultMaxConns,
+				MaxFails:    &defaultMaxFails,
+				FailTimeout: defaultFailTimeout,
+				SlowStart:   defaultSlowStart,
+				Backup:      &defaultBackup,
+				Weight:      &defaultWeight,
+				Down:        &defaultDown,
+			},
+			expected: true,
+		},
+		{
+			server: StreamUpstreamServer{
+				ID:          1,
+				Server:      "127.0.0.1",
+				MaxConns:    &defaultMaxConns,
+				MaxFails:    &defaultMaxFails,
+				FailTimeout: defaultFailTimeout,
+				SlowStart:   defaultSlowStart,
+				Backup:      &defaultBackup,
+				Weight:      &defaultWeight,
+				Down:        &defaultDown,
+			},
+			serverNGX: StreamUpstreamServer{
+				ID:          1,
+				Server:      "127.0.0.1",
+				MaxConns:    &defaultMaxConns,
+				MaxFails:    &defaultMaxFails,
+				FailTimeout: defaultFailTimeout,
+				SlowStart:   defaultSlowStart,
+				Backup:      &defaultBackup,
+				Weight:      &defaultWeight,
+				Down:        &defaultDown,
+			},
+			expected: true,
+		},
+		{
+			server:    StreamUpstreamServer{},
+			serverNGX: StreamUpstreamServer{SlowStart: "10s"},
+			expected:  false,
+		},
+		{
+			server:    StreamUpstreamServer{SlowStart: "20s"},
+			serverNGX: StreamUpstreamServer{SlowStart: "10s"},
+			expected:  false,
+		},
+	}
+
+	for _, test := range tests {
+		result := haveSameParametersForStream(test.server, test.serverNGX)
+		if result != test.expected {
+			t.Errorf("haveSameParametersForStream(%v, %v) returned %v but expected %v", test.server, test.serverNGX, result, test.expected)
 		}
 	}
 }
